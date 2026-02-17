69b9ac93287d81fc7e8640d5d155faa1d5c6a92d
Umme-Habiba123 <habibaislammim@gmail.com>
2026-02-17 18:24:45 +0600

    implement object by constructor

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const n = 0  ;
+// const n = 0  ;
+class player{
+    constructor(name){
+        console.log('calling the cobtructor', name)
+    }
+    goal(){
+        console.log('score a goal')
+    }
+}
+
+const player1 = new player('mahiya')
+const player2 =new player('mim')
+// console.log(player1)
